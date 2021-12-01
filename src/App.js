43b9080a7f2a8d3c3d1e5758bde3851e841094d7
abdelgaderalherich <abdelgaderalherich@gmail.com>
2021-12-01 12:43:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+      
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          counter: 0
+        }
+    }
+
+    increse = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+    ali=()=>{
+      if (this.state.counter >0){ 
+      this.setState({
+        counter: this.state.counter - 1
+        });
+    }
+    }
+
+    ahmed = () => {
+        this.setState({
+            counter: 0
+        })
+
+    }
+
+
+
+    render() {
+        return (
+            <div className="abd">
+              <h1>Currency Exchange </h1>
+                  <img  src="/image.jpg" alt=""/>
+                  <br/>
+                  
+                <button onClick={this.increse}  >+</button>
+                <button onClick={this.ali}>-</button>
+                <p> {this.state.counter} </p>
+                <button onClick={this.ahmed}>reset</button>
+               
+
+
+
+            </div>
+        )
+    }
+
+
+
+
+
+
+
 }
-
-export default App;
